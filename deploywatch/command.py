e3556e503e533e4f3e05c2f7fd1f33980139a9a0
hamakou108 <hamakou108@gmail.com>
2023-02-23 18:06:37 +0900

    feat: history command

--- deploywatch/command.py
+++ deploywatch/command.py
@@ -1,12 +1,53 @@
+from datetime import datetime
 from enum import Enum
+
 
 class Scope(str, Enum):
     all = "all"
     code = "code"
     deployment = "deployment"
 
-def log():
+
+class History:
+    def __init__(
+            self,
+            first_committed_at: datetime,
+            merged_at: datetime,
+            deployed_at: datetime,
+    ):
+        self.first_committed_at = first_committed_at
+        self.merged_at = merged_at
+        self.deployed_at = deployed_at
+
+    @staticmethod
+    def keys() -> list[str]:
+        return [
+            'first_committed_at',
+            'merged_at',
+            'deployed_at',
+        ]
+
+    def values(self) -> list[datetime]:
+        return [
+            self.first_committed_at,
+            self.merged_at,
+            self.deployed_at
+        ]
+
+    def __eq__(self, other: 'History'):
+        return (self.first_committed_at == other.first_committed_at
+                and self.merged_at == other.merged_at
+                and self.deployed_at == other.deployed_at)
+
+
+def generate_histories():
     # GitHub
     # CircleCI
-    # output
-    pass
+
+    return [
+        History(
+            datetime.fromisoformat('2023-02-23T10:00:00+00:00'),
+            datetime.fromisoformat('2023-02-23T11:00:00+00:00'),
+            datetime.fromisoformat('2023-02-23T11:30:00+00:00'),
+        )
+    ]
--- deploywatch/main.py
+++ deploywatch/main.py
@@ -1,11 +1,19 @@
 import typer
 from deploywatch.command import Scope
+from rich import print
+
+from command import History, generate_histories
+
 
 def main(
-    name: str,
-    scope: Scope = Scope.all
+        name: str,
+        scope: Scope = Scope.all
 ):
-    typer.echo(f"Hello {name}, {scope.value}!")
+    histories = generate_histories()
+    print(",".join(History.keys()))
+    for h in histories:
+        print(",".join([v.isoformat() for v in h.values()]))
+
 
 if __name__ == "__main__":
     typer.run(main)
