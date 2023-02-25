1b9f820844def28679d607c479665dd6b33ac33b
hamakou108 <hamakou108@gmail.com>
2023-02-25 20:17:46 +0900

    feat: specify limit (#8)

--- deploywatch/command.py
+++ deploywatch/command.py
@@ -1,22 +1,24 @@
 from dotenv import load_dotenv
 
+from datetime import datetime
+
 from deploywatch.history import History
 from deploywatch.repository import get_repository_history
 from deploywatch.deployment import get_deployment_history
 from deploywatch.presentation import write_histories
 
 
-def generate_histories(name: str, code_only: bool):
+def generate_histories(name: str, code_only: bool, limit: int):
     load_dotenv()
     histories = []
 
-    repository_histories = get_repository_history(name)
+    repository_histories = get_repository_history(name, limit)
 
     # CircleCI
     deployment_histories = []
     if not code_only:
         sha_list = [rh.merge_commit_sha for rh in repository_histories]
-        deployment_histories = get_deployment_history(name, sha_list)
+        deployment_histories = get_deployment_history(name, sha_list, limit)
 
     for rh, dh in zip(repository_histories, deployment_histories):
         histories.append(History(
